859b608ab176bf6fa18fcf1ce1635b04f977fc82
dvdrtrgn <dvdrtrgn@gmail.com>
2021-10-08 11:26:32 -0500

    Move nav stuff

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contacts">Contacts</router-link>
-    </div>
+    <AppLayoutLinks></AppLayoutLinks>
     <router-view/>
   </div>
 </template>
+
+<script>
+import AppLayoutLinks from './layouts/AppLayoutLinks.vue'
+export default {
+  components: {AppLayoutLinks}
+}
+</script>
 
 <style>
 #app {
@@ -16,15 +19,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
